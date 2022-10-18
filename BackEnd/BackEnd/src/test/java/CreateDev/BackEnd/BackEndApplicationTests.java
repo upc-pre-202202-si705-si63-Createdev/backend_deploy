3cd6ebba9a5ec46cc24a92a4e9f38f98327655a9
Pedro Alonso <73724702+PArts23@users.noreply.github.com>
2022-10-18 12:35:22 -0500

    Add files via upload

---
+++ BackEnd/BackEnd/src/test/java/CreateDev/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package CreateDev.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
