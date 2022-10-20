c951956c08c8a2e6b30874ab77bb21912e5afdc3
diiegoricse <u202016793@upc.edu.pe>
2022-10-20 01:51:10 -0500

    feat(Compra):Added complete code to class Compra

--- BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/entities/Compra.java
+++ BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/entities/Compra.java
@@ -10,18 +10,93 @@ public class Compra {
 
     }
 
-    public Compra(int idCompra,String nombreUsuario, int cantidad,float precio_total,float peso_total,
-                  int tipo_comprobante, String fecha_cancelacion){
-        this.id = idCompra;
-        this.nombre = nombreUsuario;
-
-
+    public Compra(int id, String nombre, int cantidad, float precio_total, float peso_total, int tipo_comprobante, String fecha_cancelacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio_total = precio_total;
+        this.peso_total = peso_total;
+        this.tipo_comprobante = tipo_comprobante;
+        this.fecha_cancelacion = fecha_cancelacion;
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "Nombre",length = 45,nullable = false)
+    @Column(name = "nombre",length = 55,nullable = false)
     private String nombre;
+
+    @Column(name = "cantidad",length = 55,nullable = false)
+    private int cantidad;
+
+    @Column(name = "precio_total",length = 55,nullable = false)
+    private float precio_total;
+
+    @Column(name = "peso_total",length = 55,nullable = false)
+    private float peso_total;
+
+    @Column(name = "tipo_comprobante",length = 55,nullable = false)
+    private int tipo_comprobante;
+
+    @Column(name = "fecha_cancelacion",length = 55,nullable = false)
+    private String fecha_cancelacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecio_total() {
+        return precio_total;
+    }
+
+    public void setPrecio_total(float precio_total) {
+        this.precio_total = precio_total;
+    }
+
+    public float getPeso_total() {
+        return peso_total;
+    }
+
+    public void setPeso_total(float peso_total) {
+        this.peso_total = peso_total;
+    }
+
+    public int getTipo_comprobante() {
+        return tipo_comprobante;
+    }
+
+    public void setTipo_comprobante(int tipo_comprobante) {
+        this.tipo_comprobante = tipo_comprobante;
+    }
+
+    public String getFecha_cancelacion() {
+        return fecha_cancelacion;
+    }
+
+    public void setFecha_cancelacion(String fecha_cancelacion) {
+        this.fecha_cancelacion = fecha_cancelacion;
+    }
+
+
 }
