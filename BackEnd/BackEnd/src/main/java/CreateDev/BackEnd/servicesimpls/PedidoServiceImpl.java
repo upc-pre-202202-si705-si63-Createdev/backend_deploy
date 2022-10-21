b3162ca245882e1320b324c6aa669f77204b341b
PArts23 <alonso2405.13@gmail.com>
2022-10-21 02:29:28 -0500

    Se implementaron las funciones de registrar y listar para la entidad Pedido

---
+++ BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/servicesimpls/PedidoServiceImpl.java
@@ -0,0 +1,27 @@
+package CreateDev.BackEnd.servicesimpls;
+
+import CreateDev.BackEnd.entities.Pedido;
+import CreateDev.BackEnd.repositories.IPedidoRepository;
+import CreateDev.BackEnd.serviceinterfaces.IPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoServiceImpl implements IPedidoService {
+
+    @Autowired
+    private IPedidoRepository pR;
+
+    @Override
+    public void insert(Pedido pedido) {
+        pR.save(pedido);
+    }
+
+    @Override
+    public List<Pedido> list() {
+        return pR.findAll();
+    }
+
+}
