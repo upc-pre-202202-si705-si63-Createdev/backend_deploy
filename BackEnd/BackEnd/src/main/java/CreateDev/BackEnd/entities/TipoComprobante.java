de906436c74e73006a99d3872d0ad3a4487a5d28
Sebastian Estephano Cruz Guerra <Sebass-Cruz>
2022-10-23 19:15:41 -0500

    segunda version

--- BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/entities/TipoComprobante.java
+++ BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/entities/TipoComprobante.java
@@ -1,2 +1,40 @@
-package CreateDev.BackEnd.entities;public class TipoComprobante {
+package CreateDev.BackEnd.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tipocomprobante")
+public class TipoComprobante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "comprobante", length = 45, nullable = false)
+    private String comprobante;
+
+
+    public TipoComprobante() {
+    }
+
+    public TipoComprobante (int id, String comprobante) {
+        this.id = id;
+        this.comprobante = comprobante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getcomprobante() {
+        return comprobante;
+    }
+
+    public void setcomprobante(String comprobante) {
+        this.comprobante = comprobante;
+    }
+
 }
--- BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/serviceinterfaces/ITipoComprobanteService.java
+++ BackEnd/BackEnd/src/main/java/CreateDev/BackEnd/serviceinterfaces/ITipoComprobanteService.java
@@ -1,2 +1,14 @@
-package CreateDev.BackEnd.serviceinterfaces;public interface ITipoComprobanteService {
+package CreateDev.BackEnd.serviceinterfaces;
+import CreateDev.BackEnd.entities.Pedido;
+import CreateDev.BackEnd.entities.TipoComprobante;
+
+import java.util.List;
+public interface ITipoComprobanteService {
+    public void insert(TipoComprobante tipocomprobante);
+
+    List<TipoComprobante> list();
+
+    public void delete(int id);
+
+    List<TipoComprobante> search(String comprobante);
 }
